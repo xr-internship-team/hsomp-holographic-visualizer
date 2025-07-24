cb16a05eac8eb2bf22d1f4bf3d9364ad5701060f
Deniz YALDIZ <54864981+DenizYald3iz@users.noreply.github.com>
2025-07-24 10:51:55 +0300

    Rotation Logic
    Rotation Logic

---
+++ Assets/Scripts/TestRotation.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class TestRotation : MonoBehaviour
+{
+    public Transform markerOrigin;   // marker referansı
+    public Transform worldPoint;     // world'de gösterilecek nokta
+
+    public void RotateLogger()
+    {
+        // marker referans sistemine göre tanımlı bir nokta (örneğin x=1 birim ileri)
+        Vector3 localPoint = new Vector3(1, 0, 0);
+
+        // marker'ın dünya pozisyonu
+        Vector3 T = markerOrigin.position;
+
+        // marker'ın rotasyon matrisi (Quaternion -> Matrix)
+        Matrix4x4 localToWorld = Matrix4x4.Rotate(markerOrigin.rotation);
+        print($"R: {localToWorld}");
+        Matrix4x4 worldToLocal = localToWorld.transpose; // ortonormal varsayıyoruz
+
+        // p_world = R * p_local + T
+        Vector3 transformedWorld = localToWorld.MultiplyVector(localPoint) + T;
+
+        // worldPoint objesini bu pozisyona taşı
+        worldPoint.position = transformedWorld;
+
+        // Şimdi tersini yapalım
+        Vector3 recoveredLocal = worldToLocal.MultiplyVector(transformedWorld - T);
+
+        print("Local Point: " + localPoint);
+        print("Transformed World Point: " + transformedWorld);
+        print("Recovered Local Point: " + recoveredLocal);
+    } 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            RotateLogger();
+        }
+    }
+}
